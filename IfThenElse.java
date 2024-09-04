410763878775ed24a76c56e2e4b6429e4b7a8732
MDaffaAthallahA <daff4athallah@gmail.com>
2024-09-04 08:40:48 +0700

    Add files via upload

---
+++ IfThenElse.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.github;
+
+/**
+ *
+ * @author daff4
+ */
+public class IfThenElse {
+     public static void main(String[] args) {
+        boolean isOn = false;
+
+
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        } else {
+            System.out.println("Kondisi tidak terpenuhi...");
+        }
+    }
+}
+
+    
